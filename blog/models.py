a28a9999fe357e03065a9713d7583f85551948c3
Tatsiana_Kryshtofik <tatiana-kri@tut.by>
2022-01-24 19:00:15 +0300

    deleted ugettext_lazy

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
 
 
 from .managers import UserManager
@@ -8,13 +7,13 @@ from .managers import UserManager
 
 class User(AbstractBaseUser):
 
-    email = models.EmailField(_('email'), unique=True)
-    first_name = models.CharField(_('name'), max_length=50, blank=True)
-    last_name = models.CharField(_('surname'), max_length=50, blank=True)
-    birthday = models.DateField(_('birthday'), null=True)
-    phone_number = models.CharField(_('phone_number'), max_length=12)
-    date_joined = models.DateTimeField(_('registered'), auto_now_add=True)
-    is_active = models.BooleanField(_('is_active'), default=True)
+    email = models.EmailField('email', unique=True)
+    first_name = models.CharField('name', max_length=50, blank=True)
+    last_name = models.CharField('surname', max_length=50, blank=True)
+    birthday = models.DateField('birthday', null=True)
+    phone_number = models.CharField('phone_number', max_length=12)
+    date_joined = models.DateTimeField('registered', auto_now_add=True)
+    is_active = models.BooleanField('is_active', default=True)
     avatar = models.ForeignKey('Image', on_delete=models.CASCADE)
 
     objects = UserManager()
@@ -23,8 +22,8 @@ class User(AbstractBaseUser):
     REQUIRED_FIELDS = []
 
     class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
 
     def get_full_name(self):
         full_name = '{} {}'.format(self.first_name, self.last_name)
@@ -36,33 +35,33 @@ class User(AbstractBaseUser):
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    country = models.CharField(_('country'), max_length=50)
-    city = models.CharField(_('city'), max_length=50)
-    address = models.CharField(_('address'), max_length=50)
-    phone = models.CharField(_('phone'), max_length=12)
-    created = models.DateTimeField(_('created'), auto_now_add=True)
-    updated = models.DateTimeField(_('updated'), auto_now=True)
+    country = models.CharField('country', max_length=50)
+    city = models.CharField('city', max_length=50)
+    address = models.CharField('address', max_length=50)
+    phone = models.CharField('phone', max_length=12)
+    created = models.DateTimeField('created', auto_now_add=True)
+    updated = models.DateTimeField('updated', auto_now=True)
 
     class Meta:
-        verbose_name = _('user info')
-        verbose_name_plural = _('users info')
+        verbose_name = 'user info'
+        verbose_name_plural = 'users info'
 
 
 class Post(models.Model):
 
-    user = models.ForeignKey(User, verbose_name=_('author'), on_delete=models.CASCADE)
+    user = models.ForeignKey(User, verbose_name='author', on_delete=models.CASCADE)
     category = models.ForeignKey('category', on_delete=models.SET_NULL, null=True)
     subcategory = models.ForeignKey('subcategory', on_delete=models.SET_NULL, null=True)
-    title = models.CharField(_('title'), max_length=50)
+    title = models.CharField('title', max_length=50)
     body = models.TextField('text', blank=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='post')
-    created = models.DateTimeField(_('created'), auto_now_add=True)
-    updated = models.DateTimeField(_('updated'), auto_now=True)
+    created = models.DateTimeField('created', auto_now_add=True)
+    updated = models.DateTimeField('updated', auto_now=True)
     image = models.ForeignKey('Image', on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = _('post')
-        verbose_name_plural = _('posts')
+        verbose_name = 'post'
+        verbose_name_plural = 'posts'
         ordering = ['created', 'title']
 
     def __str__(self):
@@ -71,25 +70,25 @@ class Post(models.Model):
 
 class Category(models.Model):
 
-    title = models.CharField(_('title'), max_length=50)
+    title = models.CharField('title', max_length=50)
     subtitle = models.ManyToManyField('subcategory')
-    created = models.DateTimeField(_('created'), auto_now_add=True)
+    created = models.DateTimeField('created', auto_now_add=True)
 
     class Meta:
-        verbose_name = _('category')
-        verbose_name_plural = _('categories')
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.title
 
 
 class Subcategory(models.Model):
-    title = models.CharField(_('title'), max_length=50)
-    created = models.DateTimeField(_('updated'), auto_now_add=True)
+    title = models.CharField('title', max_length=50)
+    created = models.DateTimeField('updated', auto_now_add=True)
 
     class Meta:
-        verbose_name = _('subcategory')
-        verbose_name_plural = _('subcategories')
+        verbose_name = 'subcategory'
+        verbose_name_plural = 'subcategories'
 
     def __str__(self):
         return self.title
@@ -97,12 +96,12 @@ class Subcategory(models.Model):
 
 class Tag(models.Model):
 
-    title = models.CharField(_('title'), max_length=50)
-    created = models.DateTimeField(_('updated'), auto_now_add=True)
+    title = models.CharField('title', max_length=50)
+    created = models.DateTimeField('updated', auto_now_add=True)
 
     class Meta:
-        verbose_name = _('tag')
-        verbose_name_plural = _('tags')
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
 
     def __str__(self):
         return self.title
@@ -112,19 +111,19 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     body = models.TextField('text')
-    created = models.DateTimeField(_('created'), auto_now_add=True)
-    updated = models.DateTimeField(_('updated'), auto_now=True)
+    created = models.DateTimeField('created', auto_now_add=True)
+    updated = models.DateTimeField('updated', auto_now=True)
 
     class Meta:
-        verbose_name = _('comment')
-        verbose_name_plural = _('comments')
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
 
 
 class PostRating(models.Model):
 
-    value = models.SmallIntegerField(_('value'))
-    created = models.DateTimeField(_('created'), auto_now_add=True)
-    update = models.DateTimeField(_('updated'), auto_now=True)
+    value = models.SmallIntegerField('value')
+    created = models.DateTimeField('created', auto_now_add=True)
+    update = models.DateTimeField('updated', auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
@@ -132,18 +131,18 @@ class PostRating(models.Model):
         return self.value
 
     class Meta:
-        verbose_name = _('rating')
-        verbose_name_plural = _('ratings')
+        verbose_name = 'rating'
+        verbose_name_plural = 'ratings'
 
 
 class Image(models.Model):
 
-    image_url = models.URLField(_('image_url'), max_length=50)
-    length = models.CharField(_('length'), max_length=50)
-    width = models.CharField(_('width'), max_length=50)
-    created = models.DateTimeField(_('created'), auto_now_add=True)
+    image_url = models.URLField('image_url', max_length=50)
+    length = models.CharField('length', max_length=50)
+    width = models.CharField('width', max_length=50)
+    created = models.DateTimeField('created', auto_now_add=True)
 
     class Meta:
-        verbose_name = _('image')
-        verbose_name_plural = _('images')
+        verbose_name = 'image'
+        verbose_name_plural = 'images'
 
