b355c845e6fee11bb50edcff6528d00be96bfb1b
Tatsiana_Kryshtofik <tatiana-kri@tut.by>
2022-01-24 22:57:25 +0300

    deleted normalize_email

--- blog/managers.py
+++ blog/managers.py
@@ -5,7 +5,6 @@ class UserManager(BaseUserManager):
 
 
     def _create_user(self, email, password, **extra_fields):
-        email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
